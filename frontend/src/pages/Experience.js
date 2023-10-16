b779bfdfcc928947f2807e8a9dfd07cf29b4f49f
theodore-brucker <theodore.brucker@maine.edu>
2023-10-16 11:53:26 -0400

    Added current work section to Experience page

--- frontend/src/pages/Experience.js
+++ frontend/src/pages/Experience.js
@@ -26,6 +26,8 @@ const Experience = () => {
         <h2>Research Interests - Privacy Engineering and Regulatory Compliance Lab </h2>
         Student Researcher focusing on enabling security and privacy preserving practices<br />
         <a href="https://www.sepidehghanavati.com/research">Info about PERC Lab</a>
+        <h3>Current Work</h3>
+        Natural Language Processing for task of mapping software developer questions to specific GDPR articles
         <h3>Publications</h3>
         <a href="https://arxiv.org/ftp/arxiv/papers/2306/2306.11174.pdf">[1]</a>
         Evaluating Privacy Questions From Stack Overflow: Can ChatGPT Compete?
